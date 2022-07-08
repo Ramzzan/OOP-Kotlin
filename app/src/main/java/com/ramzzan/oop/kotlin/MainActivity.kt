41b8ffc1d2a628cf06b260c8f3b3c6ad5c15731a
Waleed Ahmed <waleed.wit@gmail.com>
2022-07-08 15:52:20 +0300

    Initial commit

---
+++ app/src/main/java/com/ramzzan/oop/kotlin/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ramzzan.oop.kotlin
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.ramzzan.oop.kotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    
+}
